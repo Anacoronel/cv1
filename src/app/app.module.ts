60acaf3ca0ee693c4ed7601241ccd19e3e10ccb5
Ana Coronel <anacoronel@hotmail.com>
2021-12-15 15:10:57 -0300

    portfolio cv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
@@ -26,7 +26,8 @@ import { ProyectosComponent } from './componentes/proyectos/proyectos.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
